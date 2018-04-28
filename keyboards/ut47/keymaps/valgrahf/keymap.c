ac0e3379eacfa684bd26a53e705053643a0b4eff
root <root@DESKTOP-VRQPV4R.localdomain>
2018-04-27 20:05:58 -0500

    modified:   keyboards/ut47/keymaps/valgrahf/keymap.c 	modified:   keyboards/ut47/keymaps/valgrahf/rules.mk

--- keyboards/ut47/keymaps/valgrahf/keymap.c
+++ keyboards/ut47/keymaps/valgrahf/keymap.c
@@ -7,6 +7,7 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
+
 #define _BASE 0
 #define _FN1 1
 #define _FN2 2
@@ -20,7 +21,6 @@ enum custom_keycodes {
 #define XXXXXXX KC_NO
 #define KC_FN1 LT(_FN1, KC_NO)
 #define KC_FN2 LT(_FN2, KC_NO)
-#define KC_SPFN LT(_FN1, KC_SPACE)
 #define KC_RST RESET
 #define KC_DBUG DEBUG
 #define KC_RTOG RGB_TOG
--- keyboards/ut47/keymaps/valgrahf/rules.mk
+++ keyboards/ut47/keymaps/valgrahf/rules.mk
@@ -1,7 +1,4 @@
 RGBLIGHT_ENABLE = yes
-BACKLIGHT_ENABLE = no
-AUDIO_ENABLE = no
-USE_I2C = no
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
